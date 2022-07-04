f8864cc60be5056f3fb789213aad74dfb01a8706
urevise <soocc15@gmail.com>
2022-07-04 23:14:48 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "vscode 설정 및 단축키",
+        "excerpt":"vscode 설정 및 단축키 확장 앱 live server auto rename tag beautify bracket pair colorizer git history git graph 단축키 for Mac 환경설정 command ⌘ + , 명령어 입력 command ⌘ + shift ⇧ + p 다중 항목 선택 커서 위/아래 추가 option ⌥ + command ⌘ + 화살표 위/아래...","categories": [],
+        "tags": [],
+        "url": "/post/",
+        "teaser": null
+      }]
